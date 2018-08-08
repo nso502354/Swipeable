232230a8668b725e6a917d645a358e478fc640fd
Taehun Kim <8k4058@gmail.com>
2018-08-09 00:43:14 +0900

    프로젝트 초기화

---
+++ Swipeable/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Swipeable
+//
+//  Created by 김태헌 on 2018. 8. 8..
+//  Copyright © 2018년 김태헌. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
